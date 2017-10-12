6ed5719fe3b19e32b4bf66d92b1a9a90cd1b96be
Eliza Khachatryan <elijbet@gmail.com>
2017-10-12 13:42:36 -0700

    instance variable for grabbing url from post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
   # Create action saves the post into database
   def create
     @post = Post.new(post_params)
+    @image = LinkThumbnailer.generate(@post.url)
     if @post.save
       flash[:notice] = "Successfully created post!"
       redirect_to post_path(@post)
