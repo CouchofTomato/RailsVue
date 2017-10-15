4177de7631ea0f4a3abdcf202ff20bafdcd5e22a
Eliza Khachatryan <elijbet@gmail.com>
2017-10-15 08:43:20 -0700

    user_id to comments tb, associations, add email display to comments, change font and size for logo

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Setting up the Posts Controller and Views
 Installing CKeditor
 Image uploading for posts
 Adding Carrierwave and Mini_Magick
+Link_Thumbnailer
 Image uploading for Admins
 User authentication with Devise
 User privileges
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
 	before_action :find_commentable
+  before_action :authenticate_user!, except: [:index, :show]
 
     def new
       @comment = Comment.new
@@ -7,6 +8,7 @@ class CommentsController < ApplicationController
 
     def create
       @comment = @commentable.comments.new(comment_params)
+      @comment.user_id = current_user.id
 
       if @comment.save
         redirect_back(fallback_location: root_path, notice: 'Your comment was successfully posted!') 
